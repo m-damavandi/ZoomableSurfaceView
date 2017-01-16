5380b7f5126da314a74196b9295cd095fbed8a2c
arch <damavandi@email.com>
2017-01-16 13:28:14 +0330

    add surfaceView

--- app/src/main/java/com/damavandi/zoomablesurfaceview/MainActivity.java
+++ app/src/main/java/com/damavandi/zoomablesurfaceview/MainActivity.java
@@ -1,13 +1,43 @@
 package com.damavandi.zoomablesurfaceview;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    SurfaceView surfaceView;
+    private MediaPlayer mediaPlayer;
+    String path = "http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_10mb.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), Uri.parse(path), holder);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener(){
+            public void onPrepared(MediaPlayer arg0) {
+                mediaPlayer.start();
+            }} );
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
